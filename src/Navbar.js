3f75529e890417198b0f45d8f4134e9d0f8e0a03
mickeygray <mgray@nattaxexperts.com>
2020-05-27 15:37:13 -0700

    first commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,16 +2,23 @@ import React from "react";
 
 const Navbar = ({ icon, title }) => {
   return (
-    <nav className='navbar bg-primary'>
-      <h1 style={{ zIndex: "999999" }}>
-        <img
-          src='http://s3.amazonaws.com/dfc_attachments/images/3283575/FreshStartLogo1_web.jpg'
-          alt=''
-          className='round-img'
-          style={{ width: "75px", height: "75px" }}
-        />
-        Fresh Start Qualification Questionaire
-      </h1>
+    <nav className='navbar bg-primary' style={{ zIndex: "999999" }}>
+      <ul style={{ listStyle: "none" }}>
+        <li>
+          <img
+            src='http://s3.amazonaws.com/dfc_attachments/images/3283575/FreshStartLogo1_web.jpg'
+            alt=''
+            className='round-img'
+            style={{ width: "75px", height: "75px" }}
+          />
+        </li>
+        <li>
+          <h1>
+            <br />
+            Fresh Start Qualification Questionaire
+          </h1>
+        </li>
+      </ul>
     </nav>
   );
 };
